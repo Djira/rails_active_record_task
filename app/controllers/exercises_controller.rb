f0e6ab7ed162248b8f0af2c9507dbbd1af1c7be3
Djira <djcoully7@gmail.com>
2023-04-24 16:19:36 +0000

    first commit

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,27 +1,41 @@
 class ExercisesController < ApplicationController
   def exercise1
-    # 【要件】注文されていないすべての料理を返すこと
-    #   * left_outer_joinsを使うこと
+    # [Pour renvoyer tous les plats non commandés.
+    # Utilisez left_outer_joins.
     @foods = Food
+    .left_outer_joins(:order_foods)
+    .distinct.where(order_foods:{id: nil})
   end
 
   def exercise2
-    # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
-    #   * left_outer_joinsを使うこと
+    # [Pour retourner tous les magasins qui servent de la nourriture qui n'a pas été commandée.
+    # Pour utiliser left_outer_joins.
     @shops = Shop
+    .left_outer_joins(foods: :order_foods)
+    .distinct.where(foods:{order_foods:{id: nil}})
   end
 
   def exercise3 
-    # 【要件】配達先の一番多い住所を返すこと
-    #   * joinsを使うこと
-    #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
+    # [Pour renvoyer l'adresse de livraison la plus fréquente.
+    # Pour utiliser les jointures.
+    # Renvoyer le nombre de commandes lors de l'appel de orders_count sur une instance de l'adresse acquise.
     @address = Address
+    .joins(:orders)
+    .select("adresse.*,COUNT(orders.*)orders_count")
+    .group("addresses.id")
+    .order("orders_count DESC")
+    .first
   end
 
   def exercise4 
-    # 【要件】一番お金を使っている顧客を返すこと
-    #   * joinsを使うこと
-    #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
+    # [Pour retourner les clients qui dépensent le plus d'argent.
+    # Pour utiliser les jointures.
+    # Renvoyer le montant total de l'argent lors de l'appel à foods_price_sum sur l'instance de client récupérée.
     @customer = Customer
+    .joins("order: :foods")
+    .select("customers.*, SUM(foods.price) AS foods_price_sum")
+    .group("customers.id")
+    .order("foods_price_sum DESC")
+    .first
   end
 end
