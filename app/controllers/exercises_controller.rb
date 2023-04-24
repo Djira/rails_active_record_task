16e472ab8f94d304fdfbb149893cf98324f677cd
Djira <djcoully7@gmail.com>
2023-04-24 23:16:23 +0000

    first commit

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -20,11 +20,11 @@ class ExercisesController < ApplicationController
     # Pour utiliser les jointures.
     # Renvoyer le nombre de commandes lors de l'appel de orders_count sur une instance de l'adresse acquise.
     @address = Address
-    .joins(:orders)
-    .select("adresse.*,COUNT(orders.*)orders_count")
-    .group("addresses.id")
-    .order("orders_count DESC")
-    .first
+      .joins(:orders)
+      .select("addresses.*,COUNT(orders.*)orders_count")
+      .group("addresses.id")
+      .order("orders_count DESC")
+      .first
   end
 
   def exercise4 
@@ -32,10 +32,10 @@ class ExercisesController < ApplicationController
     # Pour utiliser les jointures.
     # Renvoyer le montant total de l'argent lors de l'appel à foods_price_sum sur l'instance de client récupérée.
     @customer = Customer
-    .joins("order: :foods")
-    .select("customers.*, SUM(foods.price) AS foods_price_sum")
-    .group("customers.id")
-    .order("foods_price_sum DESC")
-    .first
+      .joins("orders: :foods")
+      .select("customers.*, SUM(foods.price) AS foods_price_sum")
+      .group("customers.id")
+      .order("foods_price_sum DESC")
+      .first
   end
 end
